55352622c46c4fada0e3a197c52b0750e1255a1c
FidoHUN <vass.frigyes@gmail.com>
2022-06-20 17:48:09 +0200

    fix some bugs

---
+++ v2/network.js
@@ -0,0 +1,73 @@
+class NeuralNetwork{
+    constructor(neuronCounts){
+        this.levels = []
+        for(let i=0;i<neuronCounts.length-1;i++){
+            this.levels.push(new Level(neuronCounts[i], neuronCounts[i+1]))
+        }
+    }
+
+    static feedForward(givenInputs,network){
+        let outputs = Level.computeOutputValues(givenInputs, network.levels[0])
+        for(let i=1;i<network.levels.length;i++){
+            outputs = Level.computeOutputValues(outputs,network.levels[i])
+        }
+        return outputs
+    }
+}
+
+class Level{
+    constructor(inputCount,outputCount){
+        this.inputs = new Array(inputCount)
+        this.outputs = new Array(outputCount)
+        this.biases = new Array(outputCount)
+        this.weights=[] // two dimensional array
+        for(let i=0;i<inputCount;i++){
+            this.weights[i]=new Array(outputCount)
+        }
+        // we dont have values yet, this is just a shell so fare
+        //setting random weights and biases
+        Level.#randomize(this)
+        // the inputs will be coming from the cars sensors
+        // we need to compute the outputs based on these numbers
+    }
+
+    static #randomize(level){
+        for(let i=0;i<level.inputs.length;i++){
+            for(let j=0;j<level.outputs.length;j++){
+                level.weights[i][j] = Math.random()
+            }
+        }
+        for(let i=0;i<level.biases.length;i++){
+            level.biases[i] = Math.random()
+        }
+    }
+
+    static computeOutputValues(givenInputs,level){
+        // set inputs from sensor to the level inputs
+        // or from prevous level outputs to the next level inputs
+        for(let i=0;i<level.inputs.length;i++){
+            level.inputs[i]=givenInputs[i]
+        }
+        let maxSum = 0;
+        let maxIndex = 0;
+        for(let i=0;i<level.outputs.length;i++){
+            let sum = 0
+            for(let j=0;j<level.inputs.length;j++){
+                sum+=level.inputs[j]*level.weights[j][i]
+            }
+            // console.log('bias',level.biases[i])
+            // console.log('sum',sum)
+            // console.log('maxSum',maxSum)
+            if(sum>level.biases[i]){
+                if(sum>maxSum){
+                    // level.outputs[i]=1
+                    maxSum = sum
+                    maxIndex = i;
+                }
+            }
+        }
+        level.outputs.fill(0)
+        level.outputs[maxIndex]=1;
+        return level.outputs;
+    }
+}
---
+++ v2/renderer.js
@@ -0,0 +1,114 @@
+class Renderer{
+	static render(boardState, canvas, score){
+		// drawing the board
+		let ctx = canvas.getContext("2d");
+		ctx.fillStyle = "lightgray";
+       		 ctx.fillRect(0,0,canvas.width,canvas.height);
+		let pixelDiff = 25;
+
+		for(let i=0 ; i<boardState.length ; i++){
+			for (let j=0 ; j<boardState[i].length ; j++){
+				if(boardState[i][j] === 2 || boardState[i][j] === 4){
+					ctx.fillStyle = "#ffffba";
+					ctx.fillRect(j*100,i*100,100,100); 
+					ctx.fillStyle = "black";
+			        ctx.font = "bold 80px Arial";
+			        ctx.fillText("2", j*100+pixelDiff, i*100+100-pixelDiff);
+				}
+				if(boardState[i][j] === 4){
+					ctx.fillStyle = "#ffffba";
+					ctx.fillRect(j*100,i*100,100,100); 
+					ctx.fillStyle = "black";
+			        ctx.font = "1bold 80px Arial";
+			        ctx.fillText("4", j*100+pixelDiff, i*100+100-pixelDiff);
+				}
+				if(boardState[i][j] === 8 || boardState[i][j] === 16){
+					ctx.fillStyle = "#ff8000";
+					ctx.fillRect(j*100,i*100,100,100); 
+					ctx.fillStyle = "white";
+			        ctx.font = "bold 80px Arial";
+			        ctx.fillText("8", j*100+pixelDiff, i*100+100-pixelDiff);
+				}
+				if(boardState[i][j] === 16){
+					ctx.fillStyle = "#ff6700";
+					ctx.fillRect(j*100,i*100,100,100); 
+					ctx.fillStyle = "white";
+			        ctx.font = "bold 48px Arial";
+			        ctx.fillText("16", j*100+pixelDiff, i*100+95-pixelDiff);
+				}
+				if(boardState[i][j] === 32){
+					ctx.fillStyle = "#ff4a00";
+					ctx.fillRect(j*100,i*100,100,100); 
+					ctx.fillStyle = "white";
+			        ctx.font = "bold 48px Arial";
+			        ctx.fillText("32", j*100+pixelDiff, i*100+95-pixelDiff);
+				}
+				if(boardState[i][j] === 64){
+					pixelDiff = 25;
+					ctx.fillStyle = "#ff0000";
+					ctx.fillRect(j*100,i*100,100,100); 
+					ctx.fillStyle = "white";
+			        ctx.font = "bold 48px Arial";
+			        ctx.fillText("64", j*100+pixelDiff, i*100+95-pixelDiff);
+				}
+				if(boardState[i][j] === 128){
+					pixelDiff = 10;
+					ctx.fillStyle = "#ffd700";
+					ctx.fillRect(j*100,i*100,100,100); 
+					ctx.fillStyle = "white";
+			        ctx.font = "bold 40px Arial";
+			        ctx.fillText("128", j*108+pixelDiff, i*100+75-pixelDiff);
+				}
+				if(boardState[i][j] === 256){
+					pixelDiff = 10;
+					ctx.fillStyle = "#ffd700";
+					ctx.fillRect(j*100,i*100,100,100); 
+					ctx.fillStyle = "white";
+			        ctx.font = "bold 40px Arial";
+			        ctx.fillText("256", j*108+pixelDiff, i*100+75-pixelDiff);
+				}
+				if(boardState[i][j] === 512){
+					pixelDiff = 10;
+					ctx.fillStyle = "#ffd700";
+					ctx.fillRect(j*100,i*100,100,100); 
+					ctx.fillStyle = "white";
+			        ctx.font = "bold 40px Arial";
+			        ctx.fillText("512", j*108+pixelDiff, i*100+75-pixelDiff);
+				}
+				if(boardState[i][j] === 1024){
+					pixelDiff = 10;
+					ctx.fillStyle = "#ffd700";
+					ctx.fillRect(j*100,i*100,100,100); 
+					ctx.fillStyle = "white";
+			        ctx.font = "bold 35px Arial";
+			        ctx.fillText("1024", j*100+pixelDiff, i*100+75-pixelDiff);
+				}
+				if(boardState[i][j] === 2048){
+					pixelDiff = 10;
+					ctx.fillStyle = "#ffd700";
+					ctx.fillRect(j*100,i*100,100,100); 
+					ctx.fillStyle = "white";
+			        ctx.font = "bold 35px Arial";
+			        ctx.fillText("2048", j*100+pixelDiff, i*100+75-pixelDiff);
+				}
+			}
+		}
+
+		//drawing the lines
+		ctx.fillStyle = "darkgray";
+        ctx.lineWidth = 2;
+        ctx.beginPath();
+        for(let i=1 ; i<5 ; i++){
+            ctx.moveTo(i*100,0);
+            ctx.lineTo(i*100,400);
+        }
+        for(let i=1 ; i<5 ; i++){
+            ctx.moveTo(0,i*100);
+            ctx.lineTo(400,i*100);
+        }
+        ctx.stroke();
+
+		const points = document.querySelector("#points")
+		points.innerHTML = score
+	}	
+}
